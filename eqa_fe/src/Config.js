36b9caf3a76519e06c552ee7b8fae42131057dfc
bhachauk <bhanuchander210@gmail.com>
2025-02-23 21:19:10 +0530

    eqa_fe files added.

--- eqa_fe/src/Config.js
+++ eqa_fe/src/Config.js
@@ -1,9 +1,9 @@
 // config.js
 
 //dev
-export const PROD_API_URL = "http://localhost:3000"
+// export const PROD_API_URL = "http://localhost:3000"
 //prod
-//export const PROD_API_URL = "https://earthquake-analytics.vercel.app"
+export const PROD_API_URL = "https://earthquake-analytics.vercel.app"
 
 
 export function GetMetricURL(metric) {
